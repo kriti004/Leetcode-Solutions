0ed9643484d86fe73f27f033bc8dc046089f9c53
Kriti Srivastava <76968430+ALostBear4@users.noreply.github.com>
2023-01-25 05:41:05 +0530

    Time: 3 ms (97.44%), Space: 42.3 MB (84.41%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+          if(s==null) return true;
+        if(s.length()==1) return true;
+            int left=0;
+        int right=s.length()-1;
+        while(left<right){
+        
+            
+         
+            char lc = s.charAt(left);
+            char rc = s.charAt(right);
+
+            if (!Character.isLetter(lc) && !Character.isDigit(lc)) {
+                left++;
+                continue;
+            }
+
+            if (!Character.isLetter(rc) && !Character.isDigit(rc)) {
+                right--;
+                continue;
+            }
+
+            lc = Character.toLowerCase(lc);
+            rc = Character.toLowerCase(rc);
+
+            if (lc != rc) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+        
+    }
+ 
+        
+    }
